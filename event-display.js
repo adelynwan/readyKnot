e31dfc81f891f91a7b646735d19129fde15a37e5
adelynwan <adelynsmartnation@gmail.com>
2024-05-26 14:46:13 +0000

    final clean

--- event-display.js
+++ event-display.js
@@ -1,12 +1,3 @@
-localStorage.clear(); 
-
-// Clear local storage when the page loads
-window.addEventListener('load', function() {
-    localStorage.clear(); // This will remove all items from local storage
-});
-
-
-
 // for the event-details html
 
 document.addEventListener('DOMContentLoaded', function() {
